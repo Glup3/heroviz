c79369a61df3e5da111767e8d0b52569144fb8b7
Phuc Tran <glup3.tran@gmail.com>
2024-08-04 22:15:55 +0200

    display 80 years in day boxes

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,48 +1,61 @@
 import type { MetaFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import dayjs from "dayjs";
+import { useMemo } from "react";
+
+const startingDate = "2024-01-01";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: "HeroViz" },
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
 
+export const loader = async () => {
+  return json({ data: ["2024-08-04"] });
+};
+
 export default function Index() {
+  const { data } = useLoaderData<typeof loader>();
+
+  const start = dayjs(startingDate);
+  const end = start.add(80, "years");
+
+  const totalDays = end.diff(start, "days");
+
+  const datesGroupedByYear = useMemo(() => {
+    return Array.from({ length: totalDays }, (_, i) =>
+      start.add(i, "days")
+    ).reduce<Record<number, dayjs.Dayjs[]>>((acc, date) => {
+      const year = date.year();
+      if (!acc[year]) {
+        acc[year] = [];
+      }
+
+      acc[year].push(date);
+      return acc;
+    }, {});
+  }, [totalDays, start]);
+
   return (
     <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      {data}
+      <div className="max-w-3xl">
+        {Object.entries(datesGroupedByYear).map(([year, dates]) => (
+          <div key={`year-${year}`} className="mb-4">
+            <div className="flex flex-wrap gap-2">
+              {dates.map((date) => (
+                <div
+                  key={date.format("YYYY-MM-DD")}
+                  className={`size-4 bg-sky-400`}
+                />
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }

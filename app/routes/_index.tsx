d879b7eec8e41b65211579c8d2760168974ab36f
Phuc Tran <glup3.tran@gmail.com>
2024-08-05 21:20:21 +0200

    add dashboard

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,7 +2,7 @@ import type { MetaFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import dayjs from "dayjs";
-import { useMemo } from "react";
+import { TasksGrid } from "~/components/TasksGrid";
 import { db } from "~/database.server";
 
 const getAllTasks = async () => {
@@ -26,69 +26,45 @@ export const meta: MetaFunction = () => {
 
 export const loader = async () => {
   const allTasks = await getAllTasks();
-  return json({ allTasks });
+  const start = dayjs().month(0).date(1);
+  const end = start.add(1, "year");
+
+  const dates = Array.from({ length: end.diff(start, "day") }, (_, i) =>
+    start.add(i, "day").format("YYYY-MM-DD")
+  );
+
+  return json({ allTasks, dates });
 };
 
 export default function Index() {
   const data = useLoaderData<typeof loader>();
 
   return (
-    <div className="font-sans p-4">
-      <div className="flex gap-8">
+    <main className="p-4">
+      <h1 className="font-semibold text-3xl mb-4">Dashboard</h1>
+
+      <div className="grid grid-cols-2 xl:grid-cols-3 2xl:grid-cols-5 gap-4">
         {data.allTasks.map((task) => {
-          return <BoxGrid key={task.id} task={task} />;
+          return (
+            <div
+              key={`task-${task.id}`}
+              className="border p-4 shadow"
+              // style={{ borderColor: task.hexcolor }}
+            >
+              <h2 className="font-semibold">{task.name}</h2>
+              <p className="text-slate-500 text-sm mb-4">{task.description}</p>
+
+              <TasksGrid
+                dates={data.dates.map(dayjs)}
+                hexcolor={task.hexcolor}
+                accomplishedDates={
+                  new Set(task.accomplisheds.map((a) => a.date))
+                }
+              />
+            </div>
+          );
         })}
       </div>
-    </div>
+    </main>
   );
 }
-
-type Task = Awaited<ReturnType<typeof getAllTasks>>[number];
-
-const BoxGrid = (props: { task: Task }) => {
-  const start = dayjs("2024-01-01");
-  const end = start.add(5, "years");
-
-  const datesGroupedByYear = useMemo(() => {
-    return Array.from({ length: end.diff(start, "days") }, (_, i) =>
-      start.add(i, "days")
-    ).reduce<Record<number, dayjs.Dayjs[]>>((acc, date) => {
-      const year = date.year();
-      if (!acc[year]) {
-        acc[year] = [];
-      }
-
-      acc[year].push(date);
-      return acc;
-    }, {});
-  }, [start, end]);
-
-  const accomplishedDates = new Map<string, boolean>(
-    props.task.accomplisheds.map((a) => [a.date, true])
-  );
-
-  return (
-    <div className="max-w-3xl">
-      {Object.entries(datesGroupedByYear).map(([year, dates]) => (
-        <div key={`year-${year}`} className="mb-4">
-          <div className="flex flex-wrap gap-2">
-            {dates.map((date) => {
-              const key = date.format("YYYY-MM-DD");
-              return (
-                <div
-                  key={key}
-                  className={`size-4`}
-                  style={{
-                    backgroundColor: accomplishedDates.has(key)
-                      ? props.task.hexcolor
-                      : "#ddd",
-                  }}
-                />
-              );
-            })}
-          </div>
-        </div>
-      ))}
-    </div>
-  );
-};
